0b80132ca1183e93702c8ccd00116eba9bdf94c8
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-03 11:28:10 +0545

    add newton Cardle and car

--- matter.js
+++ matter.js
@@ -1,1 +1,17 @@
-console.log('helloworld');
+let Engine = Matter.Engine;
+let Render = Matter.Render;
+let World = Matter.World;
+let Bodies = Matter.Bodies;
+let Composites  = Matter.Composites;
+let engine = Engine.create();
+let render = Render.create({
+    element: document.body,
+    engine : engine
+});
+Engine.run(engine);
+Render.run(render);
+let ball = Bodies.circle(200,10,40);
+let floor = Bodies.trapezoid(340, 300,              500, 100, .9, { isStatic:               true });
+let myCar = Composites.car(390, 0, 100, 30, 40);
+let myCradle = Composites.newtonsCradle(600, 100, 5, 10, 160);
+World.add(engine.world,[ball,floor,myCar,myCradle]);
