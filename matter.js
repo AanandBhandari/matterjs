97767835fd394cbb4907858aeeb51f677a6059e5
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-03 11:40:33 +0545

    add mouse constraint

--- matter.js
+++ matter.js
@@ -11,7 +11,13 @@ let render = Render.create({
 Engine.run(engine);
 Render.run(render);
 let ball = Bodies.circle(200,10,40);
-let floor = Bodies.trapezoid(340, 300,              500, 100, .9, { isStatic:               true });
+let floor = Bodies.trapezoid(340, 300,500, 100, .9, { isStatic: true });
 let myCar = Composites.car(390, 0, 100, 30, 40);
 let myCradle = Composites.newtonsCradle(600, 100, 5, 10, 160);
-World.add(engine.world,[ball,floor,myCar,myCradle]);
+World.add(engine.world, [ball, floor, myCar, myCradle]);
+let Mouse = Matter.Mouse;
+let MouseConstraint = Matter.MouseConstraint;
+let mouse = Mouse.create(render.canvas);
+let mouseConstraint = MouseConstraint.create(engine, { mouse: mouse });
+World.add(engine.world, mouseConstraint);
+render.mouse = mouse;
